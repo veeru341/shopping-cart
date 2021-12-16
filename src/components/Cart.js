75fdd271fc6c8364fe5dad7cec992a81ba85a08b
Veeru <iamveeru1994@gmail.com>
2021-12-16 12:48:07 +0530

    second commit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Cart() {
     return (
         <div>
-            this is a cart page
+            this is a cart page and this is veerababu
         </div>
     )
 }
